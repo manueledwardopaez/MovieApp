90c568fe84477e556c39030a0d9ae4c3896f6189
manueledwardopaez <manueledwardopaez@gmail.com>
2023-12-09 20:30:34 -0400

    fix: 🐛navbar elements showing on LoginPage

--- src/ui/pages/MovieListPage.jsx
+++ src/ui/pages/MovieListPage.jsx
@@ -5,7 +5,7 @@ export const MovieListPage = () => {
   return (
     <>
       <NavBar />
-      <section style={{ backgroundColor: "#19253B" }}>
+      <section style={{ backgroundColor: "#19253B", paddingBlockEnd: "20px" }}>
         <h1 className="text-center pt-5 pb-5 text-white">Catalogue</h1>
         <Movies />
       </section>
